189db16b71b1c7b62e26d3f80706d162669b9d58
Sourabh <sourabh4pvt@gmail.com>
2024-01-02 11:43:23 +0530

    git initials

---
+++ YouTube_Playlist_Length/calculation.js
@@ -0,0 +1,101 @@
+var totalTime = {
+  seconds: 0,
+  minutes: 0,
+  hours: 0,
+};
+
+var vidCount = 0;
+var windowType;
+
+if (window.location.href.includes("playlist")) {
+  allPlaylistVideos = document.querySelectorAll("ytd-playlist-video-renderer");
+  windowType = "playlist";
+} else {
+  allPlaylistVideos = document.querySelectorAll(
+    "ytd-playlist-panel-video-renderer"
+  );
+  windowType = "panel";
+}
+
+
+//to calculate time
+allPlaylistVideos.forEach((element) => {
+  try {
+    var timelist;
+    if (windowType === "playlist") {
+      timelist = element
+        .querySelector("#overlays")
+        .querySelector("ytd-thumbnail-overlay-time-status-renderer")
+        .querySelector("span")
+        .firstChild.data.trim()
+        .split(":");
+    } else if (windowType === "panel") {
+      timelist = element.querySelector("span#text").innerHTML.split(":");
+    }
+    vidCount++;
+    console.log(timelist);
+
+    if (timelist.length === 2) {
+      totalTime.minutes += Number(timelist[0]);
+      totalTime.seconds += Number(timelist[1]);
+    } else if (timelist.length === 3) {
+      totalTime.hours += Number(timelist[0]);
+      totalTime.minutes += Number(timelist[1]);
+      totalTime.seconds += Number(timelist[2]);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//function to format time value(hh:mm:ss)
+timeCalc = (time, totalTime) => {
+  totalTime.minutes = parseInt(time / 60);
+  totalTime.seconds = parseInt(time % 60);
+  totalTime.hours = parseInt(totalTime.minutes / 60);
+  totalTime.minutes = parseInt(totalTime.minutes % 60);
+
+  let hrs, mins, secs;
+
+  hrs = totalTime.hours > 0 ? `${totalTime.hours} hours ` : ``;
+  mins = totalTime.minutes > 0 ? `${totalTime.minutes} minutes ` : ``;
+  secs = totalTime.seconds > 0 ? `${totalTime.seconds} seconds` : ``;
+
+  return hrs + mins + secs;
+};
+
+//to calculate total no. of videos
+var mvid = `No. of videos counted: ${vidCount}`;
+
+//to calculate time at 1x
+totalTime.minutes += parseInt(totalTime.seconds / 60);
+totalTime.seconds = totalTime.seconds % 60;
+totalTime.hours += parseInt(totalTime.minutes / 60);
+totalTime.minutes = totalTime.minutes % 60;
+
+var m1 = `Total time of playlist: ${totalTime.hours} hours ${totalTime.minutes} minutes ${totalTime.seconds} seconds`;
+
+var totalTimeInSec = totalTime.seconds + totalTime.minutes * 60 + totalTime.hours * 3600;
+
+//to calculate average time
+var average = parseInt(totalTimeInSec / vidCount);
+var m_avg = `Average duration of a video: ${timeCalc(average, totalTime)}`;
+
+//to calculate time at 1.5x
+var totalTimeInSec1_5 = totalTimeInSec / 1.5;
+var m1_5 = `At 1.5x: ${timeCalc(totalTimeInSec1_5, totalTime)}`;
+
+//to calculate time at 2x
+var totalTimeInSec2 = totalTimeInSec / 2;
+var m2 = `At 2x: ${timeCalc(totalTimeInSec2, totalTime)}`;
+
+var details = {
+  vid: mvid,
+  //total_vid: mtotal_vid,
+  avgTime: m_avg,
+  time1: m1,
+  time1_5: m1_5,
+  time2: m2,
+};
+
+chrome.runtime.sendMessage(details);
---
+++ YouTube_Playlist_Length/main.js
@@ -0,0 +1,35 @@
+callCalculate = (tab) => {
+  const { id, url } = tab;
+  if (
+    url.indexOf("https://www.youtube.com/playlist?") > -1 ||
+    url.match(/^.*(youtu.be\/|list=)([^#\&\?]*).*/)
+  ) {
+    chrome.scripting.executeScript({
+      target: { tabId: id },
+      files: ["calculation.js"],
+    });
+  } else {
+    document.querySelector("#p1").innerHTML =
+      "This page doesn't have a YouTube playlist.";
+    document.querySelector("footer").innerHTML = " ";
+  }
+};
+
+getCurrentTab = async () => {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+};
+
+getCurrentTab().then((tab) => {
+  callCalculate(tab);
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  document.querySelector("#v").innerHTML = request.vid;
+  //document.querySelector("#t_v").innerHTML = request.total_vid;
+  document.querySelector("#avg").innerHTML = request.avgTime;
+  document.querySelector("#p1").innerHTML = request.time1;
+  document.querySelector("#p1_5").innerHTML = request.time1_5;
+  document.querySelector("#p2").innerHTML = request.time2;
+});
